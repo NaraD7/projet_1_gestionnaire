f4dc4aae2d5d3e7eb1bf20ca68872c73d563903b
NaraD7 <165957670+NaraD7@users.noreply.github.com>
2026-01-19 16:32:56 +0100

    ajout de la fonction statistiques()

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import random
+import json
+
 def menu():
     '''
     cette fonction gère et relie toutes les autres fonction, elle retourne une fonction selon le choix de l'utilisateur
@@ -29,9 +31,9 @@ def menu():
                 score = analyser_force(mdp)
                 print(score)
             case 3:
-                ajouter_compte(mdp,score,d)
+                print(ajouter_compte(mdp,score,d))
             case 4:
-                lister_compte(d)
+                lister_compte()
             case 5:
                 rechercher_compte()
             case 6:
@@ -43,7 +45,7 @@ def menu():
 
 def generer_mdp():
     '''
-    cette fonction a générer un mot de passe et va retourner le mot de passe créer, si le mot de passe ne convient pas à l'utilisateur il va proposer de le modifier manuellement
+    cette fonction va générer un mot de passe et va retourner le mot de passe créer, si le mot de passe ne convient pas à l'utilisateur il va proposer de le modifier manuellement
     :paramètres : None
     :return:
     '''
@@ -59,7 +61,7 @@ def generer_mdp():
             caractere = random.choice(voyelle)
             mdp += caractere
             mdp += random.choice(speciaux)
-    print(mdp)
+    print(f"Mot de passe généré : {mdp}")
     mdp = modifier_mdp(mdp)
     return mdp
 
@@ -80,10 +82,19 @@ def modifier_mdp(mdp):
             print("Veuillez entrer un nombre valide.")
             continue
         if changement == 1:
-            mdp = input("entrez le nouveau mot de passe : ")
-            return mdp
+            while True:
+                try:
+                    nouveau_mdp = input("Entrez le nouveau mot de passe : ")
+                    if len(nouveau_mdp) < 8:
+                        raise ValueError("Mot de passe trop court")
+
+                    if nouveau_mdp == mdp:
+                        raise ValueError("Le mot de passe doit être différent de l'ancien")
+
+                    return nouveau_mdp
+                except ValueError as e:
+                    print(e)
         elif changement == 2:
-            mdp = mdp
             return mdp
         elif changement != 1 and changement != 2 :
             print("Veuillez entrer un nombre valide.")
@@ -93,7 +104,7 @@ def modifier_mdp(mdp):
 def analyser_force(mdp) :
     '''
     cette fonction va analyser le mot de passe générer et va retourner un score de sécurité entre 0 et 100
-    paramètres : mot de passe
+    :paramètres : mot de passe
     :return:
     '''
     score = 0
@@ -124,40 +135,43 @@ def analyser_force(mdp) :
         score += 40
     return score
 
-
-def ajouter_compte(mdp, dictionnaire_sauvegarde):
+def ajouter_compte(mdp, score, d):
     '''
     cette fonction va demander à l'utilisateur d'entrer un nom et une catégorie afin de sauvegarder
     le mot de passe avec le site qui correspond dans le fichier externe
     paramètres : mot de passe généré, score , dictionnaire "d" pour sauvegarder les infos
     :return:
     '''
-    nom_site, type_site = "", ""        # initialisation de nom_site et type_site 
-    while len(nom_site) == 0:           # tant que nom_site est vide on demande à l'utilisateur d'entrer le nom du site 
-        nom_site = str(input("Entrez le nom du site : "))
-    while len(nom_site) == 0:           # tant que nom_site est vide on demande à l'utilisateur d'entrer le type du site
-        type_site = str(input("entrez le type du site (email, réseaux, banque, ..."))
-    dictionnaire_sauvegarde[nom_site] = {       # mise à jour du dictionnaire de sauvegarde avec les infos entrées 
+    nom_site = str(input("Entrez le nom du site : "))
+    type_site = str(input("entrez le type du site (email, réseaux, banque, ..."))
+    d[nom_site] = {
         "type": type_site,
-        "mot de passe": mdp
+        "mot de passe": mdp,
+        "score" : score,
     }
-    print(dictionnaire_sauvegarde)       # print de debogage 
+    sauvegarder(d)
+    print(d)
 
-def sauvegarder(dictionnaire_sauvegarde):
-    with open("sauvegarder_compte.json","w", encoding="utf-8") as compte_ecriture:      # cette ligne ouvre le fichier de sauvegarde json ("w") et y sauvegarde les sites sauvegardés par l'utilisateur 
-        json.dump(dictionnaire_sauvegarde, compte_ecriture, indent = 2)
+def sauvegarder(d):
+    with open("sauvegarde_compte.json", "w", encoding="utf-8") as fichier:
+        json.dump(d, fichier,indent = 2)
 
-def lister_compte(dictionnaire_sauvegarde):
+
+def lister_compte():
     '''
     cette fonction va afficher les comptes présent dans le fichier externe
+    paramètres : None
     :return:
     '''
-    print("--------------------------------------------")
-    for cle, valeur in d.items():
-        print(cle)
-        for cle2 in valeur:
-            print (cle2,":",valeur[cle2])
-    print("--------------------------------------------")
+
+    with open("sauvegarde_compte.json", "r", encoding="utf-8") as fichier:
+        compte = json.load(fichier)
+        for cle, valeur in compte.items():
+            print("___________________________")
+            print(cle)
+            for cle2 in valeur:
+                print (cle2, ": ", valeur[cle2])
+    print("___________________________")
 
 
 
@@ -166,5 +180,14 @@ def statistiques():
     cette fonction va afficher le score moyen de tous les mots de passe de l'utilisateur
     :return:
     '''
+    moyenne = 0
+    with open("sauvegarde_compte.json", "r", encoding="utf-8") as fichier:
+        compte = json.load(fichier)
+        for cle, valeur in compte.items():
+            moyenne += valeur["score"]
+    moyenne = moyenne / len(compte)
+    print(f"La moyenne des scores des mots de passe est de {moyenne}")
+    return moyenne
 
 
+print(menu())
