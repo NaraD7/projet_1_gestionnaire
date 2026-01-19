539b6201a6d5069ef7d0fbe4dae5818415e57842
NaraD7 <165957670+NaraD7@users.noreply.github.com>
2026-01-19 18:44:11 +0100

    ajout de la fonction maj_mdp() et de la moyenne

--- main.py
+++ main.py
@@ -7,37 +7,53 @@ def menu():
     :paramètres : None
     :return:
     '''
-    d = {}
+    d = {'utvu': {'type': 'ycuyv', 'mot de passe': 'PU6Ke4cu$wy%te2je&NE9cA@tA&', 'score': 100},
+         'youtube': {'type': 'ycuyv', 'mot de passe': 'PU6Ke4cu$wy%te2je&NE9cA@tA&', 'score': 100},
+         'twitch': {'type': 'ycuyv', 'mot de passe': 'PU6Ke4cu$wy%te2je&NE9cA@tA&', 'score': 100},
+         'steam': {'type': 'ycuyv', 'mot de passe': 'PU6Ke4cu$wy%te2je&NE9cA@tA&', 'score': 100},
+         'instagram': {'type': 'ycuyv', 'mot de passe': 'PU6Ke4cu$wy%te2je&NE9cA@tA&', 'score': 100},
+         'X': {'type': 'ycuyv', 'mot de passe': 'PU6Ke4cu$wy%te2je&NE9cA@tA&', 'score': 100},
+         'facebook': {'type': 'ycuyv', 'mot de passe': 'PU6Ke4cu$wy%te2je&NE9cA@tA&', 'score': 100},
+         'lol': {'type': 'ycuyv', 'mot de passe': 'PU6Ke4cu$wy%te2je&NE9cA@tA&', 'score': 100},
+         'riot': {'type': 'ycuyv', 'mot de passe': 'PU6Ke4cu$wy%te2je&NE9cA@tA&', 'score': 100},}
     while True:
         print("\n--- MENU TÂCHES ---\n"
               "1. Générer\n"
               "2. Analyser\n"
               "3. Ajouter compte\n"
               "4. Lister comptes\n"
-              "5. Statistiques\n"
-              "6. Quitter")
+              "5. Rechercher\n"
+              "6. Statistiques\n"
+              "7. Modifier un mot de passe\n"
+              "8. Quitter")
         while True:
             try:
                 choix = int(input("Votre choix : "))
-                break
+                if choix < 1 or choix > 7:
+                    raise ValueError
+                break  # choix valide → on sort de la boucle
             except ValueError:
-                print("Veuillez entrer un nombre valide.")
+                print("Veuillez entrer un nombre entre 1 et 7.")
+
         match choix:
             case 1 :
                 mdp = generer_mdp()
                 print(mdp)
             case 2:
                 score = analyser_force(mdp)
-                print(score)
+                print(f"score : {score}/100")
             case 3:
                 print(ajouter_compte(mdp,score,d))
             case 4:
                 lister_compte()
             case 5:
-                print(statistique())
+                rechercher_compte()
             case 6:
+                print(statistiques_compte())
+            case 7:
+                print(maj_mdp(d))
+            case 8:
                 break
-
 
 
 def generer_mdp():
@@ -59,11 +75,8 @@ def generer_mdp():
             mdp += caractere
             mdp += random.choice(speciaux)
     print(f"Mot de passe généré : {mdp}")
-    mdp = modifier_mdp(mdp)
+    mdp = modifier_mdp()
     return mdp
-
-
-
 
 
 def modifier_mdp(mdp):
@@ -73,29 +86,35 @@ def modifier_mdp(mdp):
     :return: mot de passe modifié
     '''
     while True:
-        try :
-            changement = int(input("voulez vous changer le mot de passe ? 1 : Oui,  2 : Non   : "))
-        except ValueError:
-            print("Veuillez entrer un nombre valide.")
-            continue
-        if changement == 1:
-            while True:
-                try:
-                    nouveau_mdp = input("Entrez le nouveau mot de passe : ")
-                    if len(nouveau_mdp) < 8:
-                        raise ValueError("Mot de passe trop court")
+        while True:
+            try:
+                nouveau_mdp = input("Entrez le nouveau mot de passe : ")
+                if len(nouveau_mdp) < 8:
+                    raise ValueError("Mot de passe trop court")
 
-                    if nouveau_mdp == mdp:
-                        raise ValueError("Le mot de passe doit être différent de l'ancien")
+                if nouveau_mdp == mdp:
+                    raise ValueError("Le mot de passe doit être différent de l'ancien")
 
-                    return nouveau_mdp
-                except ValueError as e:
-                    print(e)
-        elif changement == 2:
-            return mdp
-        elif changement != 1 and changement != 2 :
-            print("Veuillez entrer un nombre valide.")
+                return nouveau_mdp
+            except ValueError as e:
+                print(e)
 
+
+def maj_mdp(dictionnaire_sauvegarde):
+    '''
+    cette fonction va demander à l'utilisateur le site dont il voudra modifier le
+    mot de passe et va mettre à jour le dictionnaire des comptes et le sauvegarder sur le JSON
+    :param dictionnaire_sauvegarde:
+    :return:
+    '''
+    lister_compte()
+    modif = input("De quel site voulez vous modifier le mot de passe ?")
+    if modif not in dictionnaire_sauvegarde:                    # vérifie si le nom du site entré est présent dans le fichier de sauvegarde
+        print("Ce site n'est pas enregistré dans le gestionnaire.")
+        return maj_mdp(dictionnaire_sauvegarde)                 # relance la fonction si le site n'est pas présent
+    else:
+        dictionnaire_sauvegarde[modif]["mot de passe"] = modifier_mdp(dictionnaire_sauvegarde[modif]["mot de passe"]) # ici l'instruction est de modifier le mot de passe du site voulu en utilisant la fonction modifier_mdp()
+    sauvegarder(dictionnaire_sauvegarde)
 
 
 def analyser_force(mdp) :
@@ -107,7 +126,7 @@ def analyser_force(mdp) :
     score = 0
     speciaux = ['!', '@', '#', '$', '%', '&', '*', '?', '2', '3', '4', '5', '6', '7', '8', '9']
     nombre, majuscule, minuscule , special = False, False, False, False
-    for i in mdp :
+    for i in mdp :                              # cette boucle va vérifier la présence de majuscules (is.upper()), de minuscules (is.lower()) de chiffres et de caractères spéciaux dans le mot de passe
         if i.isdigit() and nombre == False:
             nombre = True
             score += 10
@@ -150,7 +169,7 @@ def ajouter_compte(mdp, score, d):
     print(d)
 
 def sauvegarder(d):
-    with open("sauvegarde_compte.json", "w", encoding="utf-8") as fichier:
+    with open("sauvegarde_compte.json", "wb", encoding="utf-8") as fichier:
         json.dump(d, fichier,indent = 2)
 
 
@@ -163,13 +182,12 @@ def lister_compte():
 
     with open("sauvegarde_compte.json", "r", encoding="utf-8") as fichier:
         compte = json.load(fichier)
-        for cle, valeur in compte.items():
+        for cle, valeur in compte.items():      # cette boucle parcourt le fichier json et affiche les éléments qui y sont présents
             print("___________________________")
             print(cle)
             for cle2 in valeur:
                 print (cle2, ": ", valeur[cle2])
     print("___________________________")
-
 
 
 def statistiques():
